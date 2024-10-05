d52cc24556062445d36580ac2ed44863a98e645f
Dmitry <borodulkin_d@mail.ru>
2024-10-05 16:52:51 +0300

    Placed all the elements on two view controllers, made a segway with a button.

---
+++ NamesCompApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NamesCompApp
+//
+//  Created by Дмитрий Бородулькин on 29.09.2024.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    @IBOutlet weak var yourNameTF: UITextField!
+    @IBOutlet weak var partnerNameTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     
+    }
+
+    @IBAction func resultButtonTapped() {
+    }
+    
+}
+
---
+++ NamesCompApp/ResultViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ResultViewController.swift
+//  NamesCompApp
+//
+//  Created by Дмитрий Бородулькин on 30.09.2024.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    @IBOutlet weak var namesLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var progresView: UIProgressView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
